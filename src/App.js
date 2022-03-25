9dca02f7b94400d45cff40d0ba204926a4c25b34
Alsha Nabilla <alshanabilla17@gmail.com>
2022-03-25 14:40:21 +0700

    homework m2s2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="app">
       <h1 className="title">Create Playlist</h1>
-      <PlaylistContainer />
+      <PlaylistContainer
+      />
     </div>
   );
 }
