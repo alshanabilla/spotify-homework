62f5965ba6d62c274ed776d318664e2ee9aa10b3
Alsha Nabilla <alshanabilla17@gmail.com>
2022-03-30 14:05:27 +0700

    create search song

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PlaylistContainer from './components/PlaylistContainer.js';
 function App() {
   return (
     <div className="app">
-      <h1 className="title">Create Playlist</h1>
+      <h1 className="title">Playlist</h1>
       <PlaylistContainer
       />
     </div>
